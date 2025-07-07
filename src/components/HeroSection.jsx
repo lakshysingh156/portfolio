50279fa9babdab0693ad99bdd0e5bf60f2978d37
lakshysingh156 <lakshysingh156@gmail.com>
2025-07-07 19:51:10 +1000

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,17 +12,17 @@ export const HeroSection = () => {
             <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
             <span className="text-primary opacity-0 animate-fade-in-delay-1">
               {" "}
-              Pedro
+              Lakshay
             </span>
             <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
               {" "}
-              Machado
+              Singh
             </span>
           </h1>
 
           <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
             I create stellar web experiences with modern technologies.
-            Specializing in front-end development, I build interfaces that are
+            I build interfaces that are
             both beautiful and functional.
           </p>
 
